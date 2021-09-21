20219ac1751551befcc171f3f1f168334ccb2879
Victor Manuel Sanchez Sanchez <vicool275@gmail.com>
2021-09-20 21:36:41 -0500

    Interface
    Adding a simple interface

--- Main.py
+++ Main.py
@@ -1,3 +1,15 @@
 import strategy.genetic_algorithm as ga
 
-ga.init(p_type=3)
+while True:
+    print('Estrategia: '
+          '\n\tAlgorimos geneticos'
+          '\nProblemas:'
+          '\n\t1) N Reinas'
+          '\n\t2) Mochila'
+          '\n\t3) Minimizacion de la funcion de la parabola'
+          '\n\t4) Salir')
+    problem = int(input('Escoja el numero del problema: '))
+    if problem == 4:
+        break
+    else:
+        ga.init(p_type=problem)
